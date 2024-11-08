5c976327be2147b7054835cc1dbe1bf258f8b90b
Divish <divish_28@hotmail.com>
2024-11-07 22:46:09 -0500

    Update NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,37 @@
 import React from "react";
-import styles from './NavBar.module.css';
+import styles from "./NavBar.module.css";
 
 const NavBar: React.FC = () => {
-    return (
-        <nav className={styles.Nav_Wrapper}>
-            <a href="#">divish dance</a>
-        </nav>
-    )
-}
+  return (
+    <nav className={styles.Nav_Wrapper}>
+      <a href="#">divish dance</a>
 
-export default NavBar
+      {/* Hamburger mobile menu (top right) */}
+      <input
+        type="checkbox"
+        className={styles['side-menu']}
+        id="side-menu"
+        aria-label="Open Menu"
+      />
+      <label htmlFor="side-menu" className={styles.hamb}>
+          <span className={styles['hamb-line']}></span>
+          <span className={styles['hamb-line']}></span>
+          <span className={styles['hamb-line']}></span>
+      </label>
+
+      <div className={styles.nav}>
+        <label className={styles.close} htmlFor="side-menu">
+            X
+        </label>
+        <ul className={styles.menu}>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+      </div>
+
+    </nav>
+  );
+};
+
+export default NavBar;
