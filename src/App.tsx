26e5eedaa9106db89eaf50ec377491b14e05db14
Divish <divish_28@hotmail.com>
2024-11-08 21:00:45 -0500

    Update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import NavBar from './components/NavBar/NavBar'
-import HomeIntro from './components/Main/HomeIntro'
-import './App.css'
+import NavBar from "./components/NavBar/NavBar";
+import HomeIntro from "./components/Main/HomeIntro";
+import DanceSchedule from "./components/DanceSchedule/DanceSchedule";
+
+import "./App.css";
+
+// Sample data
+const scheduleData = [
+  { day: "Mon", time: "6:00 PM - 7:00 PM", type: "Bollywood" },
+  { day: "Wed", time: "7:00 PM - 8:00 PM", type: "Persian" },
+  { day: "Fri", time: "5:30 PM - 6:30 PM", type: "Kurdish" },
+];
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <NavBar />
       <HomeIntro />
+      <DanceSchedule schedule={scheduleData} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
