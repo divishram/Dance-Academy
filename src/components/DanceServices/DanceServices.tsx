6292d866952dab332fb7866f85ced1e36c421a94
Divish <divish_28@hotmail.com>
2024-12-18 19:09:50 -0500

    Update dance service component

--- src/components/DanceServices/DanceServices.tsx
+++ src/components/DanceServices/DanceServices.tsx
@@ -1,34 +1,45 @@
 import React from "react";
 import styles from "./DanceServices.module.css";
-import ServiceCard from "./ServiceCard";
 
-interface Service {
-  id: string;
-  imageSrc: string;
-  imageAlt: string;
-  title: string;
-  description: string;
-}
-
-
-interface ServicesSectionProps {
-  services: Service[];
-}
-
-const DanceServices: React.FC<ServicesSectionProps> = ({ services }) => {
+const DanceServices: React.FC = () => {
   return (
-    <section className={styles['services']}>
+    <section className={styles["services"]}>
       <h2>Our Services</h2>
-      <div className={styles['service-cards']}>
-        {services.map((service) => (
-          <ServiceCard
-            key={service.id}
-            imageSrc={service.imageSrc}
-            imageAlt={service.imageAlt}
-            title={service.title}
-            description={service.description}
+      <div className={styles["service-cards"]}>
+        <div className={styles["service-card"]}>
+          <img
+            className={styles.image}
+            src="images/bollywood-dance.webp"
+            alt="2 Indian women in cultural dance clothing"
+            loading="lazy"
           />
-        ))}
+          <h3>Bollywood Dance</h3>
+          <p>
+            Learn the energetic form of Bollywood dance, combining classical
+            Indian dance with contemporary moves.
+          </p>
+        </div>
+        <div className={styles["service-card"]}>
+          <img
+            src="images/persian-dance.webp"
+            alt="Persians dancing with traditional clothing. Iranian flag on the right side"
+            loading="lazy"
+          />
+          <h3>Persian Dance</h3>
+          <p>Experience the graceful choreography of Persian dance.</p>
+        </div>
+        <div className={styles["service-card"]}>
+          <img
+            src="images/kurdish-dance.webp"
+            alt="Kurdish woman wearing red clothing with hand on her hip"
+            loading="lazy"
+          />
+          <h3>Kurdish Dance</h3>
+          <p>
+            Try the unique rhythms of Kurdish dance, a blend of ancient
+            traditions and joyful choreography.
+          </p>
+        </div>
       </div>
     </section>
   );
