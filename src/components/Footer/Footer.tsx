0d56cb27064455ddb3b44187b88b629a9a8a7351
Divish <divish_28@hotmail.com>
2025-04-07 21:53:47 -0400

    Fix CSS naming hashed issue

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 
 const Footer: React.FC = () => {
   return (
-    <footer id={styles["footer"]}>
+    <footer id="footer">
       <div className={styles["footer-content"]}>
         <h3>About Us</h3>
         <p>
@@ -12,8 +12,8 @@ const Footer: React.FC = () => {
         </p>
       </div>
 
-      <div className={styles['footer-section links']}>
-        <h3>Quick LInks</h3>
+      <div className={`${styles['footer-section']} ${styles['links']}`}>
+        <h3>Quick Links</h3>
         <ul>
             <li><a href="#gallery">Gallery</a></li>
             <li><a href="#about">About</a></li>
@@ -22,7 +22,7 @@ const Footer: React.FC = () => {
         </ul>
       </div>
 
-      <div className={styles['footer-section contact']}>
+      <div className={`${styles['footer-section']} ${styles['contact']}`}>
         <h3>Contact Us</h3>
         <p>Email: divish@dance.com</p>
         <p>Phone: +123 456 7890</p>
