325801277c4b07100f52a928eed6e4f29d34426a
Divish <divish_28@hotmail.com>
2025-04-18 20:03:52 -0400

    Add spinner loading

--- frontend/src/components/Main/BookClassModal.tsx
+++ frontend/src/components/Main/BookClassModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from './UI/Modal';
 import styles from './Modal.module.css'
 
@@ -8,9 +8,41 @@ interface Props {
 }
 
 const BookClassModal: React.FC<Props> = ({isOpen, onClose}) => {
-    const formSubmission = (e: React.FormEvent) => {
+    
+    // Store form data 
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        type: ""
+    })
+    const [isLoading, setIsLoading] = useState(false);
+    
+    // const [response, setResponse] = useState<string | null>(null);
+    // input changes
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const {name, value} = e.target;
+        setFormData(prev => ({...prev, [name]: value}));
+    };
+
+    const formSubmission = async (e: React.FormEvent) => {
         e.preventDefault();
-        // Todo api call to send data to back-end
+        setIsLoading(true);
+        const {name, email, type} = formData;
+        try {
+            const response = await fetch('http://localhost:3000/api/book-class', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({name, email, type}),
+            });
+            const data = await response.json();
+            console.log('Server response:', data);
+
+            
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setIsLoading(false);
+        }
         onClose(); // close form
     };
 
@@ -24,17 +56,17 @@ const BookClassModal: React.FC<Props> = ({isOpen, onClose}) => {
 
             <div>
                 <label htmlFor="name">Name</label>
-                <input id="name" required />
+                <input id="name" name="name" value={formData.name} onChange={handleInputChange} required />
             </div>
 
             <div>
                 <label htmlFor="email">Email</label>
-                <input id="email" type="email" required />
+                <input id="email" name="email" value={formData.email} onChange={handleInputChange} type="email" required />
             </div>
 
             <div>
                 <label htmlFor="type">Class Type</label>
-                <select id="type" required>
+                <select id="type" name="type" value={formData.type} onChange={handleInputChange} required>
                     <option value="">Select</option>
                     <option value="bollywood">Bollywood</option>
                     <option value="persian">Persian</option>
@@ -43,7 +75,8 @@ const BookClassModal: React.FC<Props> = ({isOpen, onClose}) => {
             </div>
 
             <div style={{ textAlign: "right" }}>
-                <button type="submit">Submit</button>
+                <button type="submit" disabled={isLoading}>
+                    {isLoading ? <div className={styles["spinner"]}></div> : "Submit"}</button>
             </div>
 
             </form>
