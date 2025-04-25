dae98c7e47026e548526d9f1b311fb7bfb3dcdd6
Divish <divish_28@hotmail.com>
2025-04-24 22:35:03 -0400

    Limit body size

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT
 const app = express();
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '1mb'}));
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
